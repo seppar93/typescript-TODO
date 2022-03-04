cfcd4581244131bd2db54b0a3a616929e6ea6a2f
Sepehr Parirokh <sepehrparirokh@gmail.com>
2022-03-04 11:04:52 -0500

    set fetch call && display

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { TodoList } from './components/TodoList';
 
+// https://jsonplaceholder.typicode.com/users/1/todos
 function App() {
   return (
     <div className="App">
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,7 +1,41 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+
+interface ITodo {
+  userId: number,
+  id:number,
+  title:string,
+  completed:boolean
+}
 
 export const TodoList = () => {
+  const [todos, setTodos] = useState<null|ITodo[]>(null)
+  const url = 'https://jsonplaceholder.typicode.com/users/1/todos'
+  async function fetchTodos() {
+    const response = await fetch(url)
+    const data = await response.json()
+    console.log(data);
+    
+    setTodos(data)
+  }
+
+  useEffect(() => {
+    fetchTodos()
+  },[])
+
+
   return (
-    <div>TodoList</div>
+    <>
+    <h3>Your current TODOS </h3>
+
+    <ul>
+      {
+        todos?.map((todo) => {
+         return  <li key={todo.id}>{todo.title}</li>
+        }  )
+      }
+    </ul>
+
+    </>
+
   )
 }
